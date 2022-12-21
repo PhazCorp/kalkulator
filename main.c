7c0ea0ab0c2b99d6ff9d571124a27d84ac7fd7d0
Sophonie Bouye <bouyesophonie@gmail.com>
2022-12-21 17:40:01 +0000

    Ready, go

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+  int firstNumber;
+  int secondNumber = 0;
+
+  printf("What is the first number ? ");
+  scanf("%d", &firstNumber);
+  printf("%d \n", firstNumber);
+  printf("What is the second number ? ");
+  scanf("%d", &secondNumber);
+  printf("%d \n", secondNumber);
+
+
+  
+  return 0;
+}
